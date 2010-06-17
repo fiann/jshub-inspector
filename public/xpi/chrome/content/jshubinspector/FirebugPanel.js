927d1569c7e408af4ae6354ffd2034dadd0ba1a3
fiann <fiann@ohagan.me>
2010-06-17 10:27:39 +0100

    Updated license
    There's no code left from the original empty extension I started from at http://code.google.com/p/firephp/downloads/detail?name=FirebugTestExtension-0.0.2.xpi

--- public/xpi/chrome/content/jshubinspector/FirebugPanel.js
+++ public/xpi/chrome/content/jshubinspector/FirebugPanel.js
@@ -1,5 +1,5 @@
 /* ***** BEGIN LICENSE BLOCK *****
- * Version: MPL 1.1/GPL 2.0/LGPL 2.1
+ * Version: MPL 1.1
  *
  * The contents of this file are subject to the Mozilla Public License Version
  * 1.1 (the "License"); you may not use this file except in compliance with
@@ -11,25 +11,16 @@
  * for the specific language governing rights and limitations under the
  * License.
  *
- * The Initial Developer of the Original Code is Christoph Dorn.
+ * The Initial Developer of the Original Code is Fiann O'Hagan.
  *
- * Portions created by the Initial Developer are Copyright (C) 2006
+ * Portions created by the Initial Developer are Copyright (C) 2010
  * the Initial Developer. All Rights Reserved.
  *
  * Contributor(s):
- *     Christoph Dorn <christoph@christophdorn.com>
+ *     Fiann O'Hagan <fiann@ohagan.me>
  *
- * Alternatively, the contents of this file may be used under the terms of
- * either the GNU General Public License Version 2 or later (the "GPL"), or
- * the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
- * in which case the provisions of the GPL or the LGPL are applicable instead
- * of those above. If you wish to allow use of your version of this file only
- * under the terms of either the GPL or the LGPL, and not to allow others to
- * use your version of this file under the terms of the MPL, indicate your
- * decision by deleting the provisions above and replace them with the notice
- * and other provisions required by the GPL or the LGPL. If you do not delete
- * the provisions above, a recipient may use your version of this file under
- * the terms of any one of the MPL, the GPL or the LGPL.
+ * Many thanks for Jan Odvarko's excellent tutorial at
+ * http://www.softwareishard.com/blog/firebug-tutorial/
  *
  * ***** END LICENSE BLOCK ***** */
 
